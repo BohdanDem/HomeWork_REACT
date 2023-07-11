3af73a41843468257228a205b9d5a54c90621e64
BohdanDem <bogdandemchuk.1@gmail.com>
2023-07-11 19:55:55 +0300

    HW_3_task_3

--- task/src/App.js
+++ task/src/App.js
@@ -3,10 +3,12 @@ import Child1 from "./Component/Child/Child_1/Child_1";
 import Child2 from "./Component/Child/Child_2/Child_2";
 
 function App() {
+    const {NewName, NewUsername, NewEmail} = null
+
   return (
     <>
       <Child1/>
-      <Child2/>
+      <Child2 NewName={NewName} NewUsername={NewUsername} NewEmail={NewEmail}/>
     </>
   );
 }
