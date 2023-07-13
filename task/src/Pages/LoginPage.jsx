ff81780f46830977f7fab171ed79b77d0016b4c3
BohdanDem <bogdandemchuk.1@gmail.com>
2023-07-13 16:27:17 +0300

    HW_4_task_2 ready for review

--- task/src/Pages/LoginPage.jsx
+++ task/src/Pages/LoginPage.jsx
@@ -1,24 +1,22 @@
-import React, {useState} from 'react';
+import React, {useRef} from 'react';
 import styles from './LoginPage.module.css'
 
 const LoginPage = () => {
-    const [formValue, setFormValue] = useState({
-        name: '',
-        username: '',
-        email: '',
-        website: ''
-    })
+
+    const nameRef = useRef(null)
+    const usernameRef = useRef(null)
+    const emailRef = useRef(null)
+    const websiteRef = useRef(null)
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        //console.log(formValue);
         fetch('https://jsonplaceholder.typicode.com/users', {
             method: 'POST',
             body: JSON.stringify({
-                name: formValue.name,
-                username: formValue.username,
-                email: formValue.email,
-                website: formValue.website
+                name: nameRef.current.value,
+                username: usernameRef.current.value,
+                email: emailRef.current.value,
+                website: websiteRef.current.value
             }),
             headers: {
                 'Content-type': 'application/json; charset=UTF-8',
@@ -34,19 +32,19 @@ const LoginPage = () => {
                 <h3>Create new user</h3>
                 <div className={styles.formGroup}>
                     <label>Name</label>
-                    <input type = "text" ref={'nameRef'}/>
+                    <input type = "text" ref={nameRef}/>
                 </div>
                 <div className={styles.formGroup}>
                     <label>UserName</label>
-                    <input type = "text" ref={'usernameRef'}/>
+                    <input type = "text" ref={usernameRef}/>
                 </div>
                 <div className={styles.formGroup}>
                     <label>Email</label>
-                    <input type = "email" ref={'emailRef'}/>
+                    <input type = "email" ref={emailRef}/>
                 </div>
                 <div className={styles.formGroup}>
                     <label>WebSite</label>
-                    <input type = "text" ref={'websiteRef'}/>
+                    <input type = "text" ref={websiteRef}/>
                 </div>
                 <input type={"submit"} value={'Create new user'}/>
             </form>
