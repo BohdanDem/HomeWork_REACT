50ed1d164702298e89b8a604689cdbfd7b5ada5d
BohdanDem <bogdandemchuk.1@gmail.com>
2023-07-18 16:11:40 +0300

    HW_5_task_1 ready for review

---
+++ task/src/Api/Endpoints.js
@@ -0,0 +1,5 @@
+export const Endpoints = {
+    USERS: '/users'
+}
+
+export const baseURL = 'https://jsonplaceholder.typicode.com'
--- task/src/Services/ApiServices.js
+++ task/src/Services/ApiServices.js
@@ -1,15 +1,16 @@
 import axios from "axios";
+import {baseURL, Endpoints} from "../Api/Endpoints";
 
 export const GetUsers = {
     getUsers: async (setUsers) => {
-        let response = await axios.get('https://jsonplaceholder.typicode.com/users')
+        let response = await axios.get(baseURL + Endpoints.USERS)
         setUsers(response.data);
     }
 }
 
 export const GetNewUsers = {
     getNewUsers: async (user, setUsers, reset) => {
-        let response = await axios.post('https://jsonplaceholder.typicode.com/users', user)
+        let response = await axios.post(baseURL + Endpoints.USERS, user)
         setUsers(prev => [...prev, response.data])
         reset()
     }
