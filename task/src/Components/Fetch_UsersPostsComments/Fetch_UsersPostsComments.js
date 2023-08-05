98cebf3e0ce6cd4c47ecaae63a41588440e854f5
BohdanDem <bogdandemchuk.1@gmail.com>
2023-08-05 21:24:21 +0300

    HomeWork_8 useFetch

--- task/src/Components/Fetch_UsersPostsComments/Fetch_UsersPostsComments.js
+++ task/src/Components/Fetch_UsersPostsComments/Fetch_UsersPostsComments.js
@@ -1,50 +1,46 @@
-import React, {useEffect, useState} from 'react';
 import styles from './Fetch_UsersPostsComments.module.css'
 import useFetch from "../../hooks/useFetch";
 
 const FetchUsersPostsComments = () => {
 
-    const [endpoint, setEndpoint] = useState( '/users')
-    const [users, setUsers] = useState([])
-    const [posts, setPosts] = useState([])
-    const [comments, setComments] = useState([])
-    // console.log(endpoint, typeof endpoint);
+    //const [endpoint, setEndpoint] = useState( null)
+    const  users = useFetch('/users')
+    const  posts = useFetch('/posts')
+    const  comments = useFetch('/comments')
 
-    const AddEndpoint = (data) => {
-        setEndpoint(data)
-    }
-    useEffect(() => {}, [endpoint])
-
-    useFetch(endpoint, setUsers, setPosts, setComments)
+    // const AddEndpoint = (data) => {
+    //     setEndpoint(data)
+    // }
+    // useEffect(() => {}, [endpoint])
 
     return (
         <div className={styles.btn}>
-            <button onClick={() => AddEndpoint('/users')}>Get Users</button>
-            <button onClick={() => AddEndpoint('/posts')}>Get Posts</button>
-            <button onClick={() => AddEndpoint('/comments')}>Get Comments</button>
+            {/*<button onClick={() => AddEndpoint('/users')}>Get Users</button>*/}
+            {/*<button onClick={() => AddEndpoint('/posts')}>Get Posts</button>*/}
+            {/*<button onClick={() => AddEndpoint('/comments')}>Get Comments</button>*/}
             <div>
                 {users.map((user) =>
-                    <div>
-                        {user.name}
-                        {user.username}
+                    <div key={user.id} className={styles.user}>
+                        <div>{user.name}</div>
+                        <div>{user.username}</div>
                     </div>
                 )}
             </div>
             <div>
                 {posts.map((post) =>
-                    <div>
-                        {post.id}
-                        {post.title}
-                        {comments.body}
+                    <div key={post.id} className={styles.post}>
+                        <div>{post.id}</div>
+                        <div>{post.title}</div>
+                        <div>{comments.body}</div>
                     </div>
                 )}
             </div>
             <div>
                 {comments.map((comment) =>
-                    <div>
-                        {comments.id}
-                        {comments.name}
-                        {comments.body}
+                    <div key={comment.id} className={styles.comment}>
+                        <div>{comment.id}</div>
+                        <div>{comment.name}</div>
+                        <div>{comment.body}</div>
                     </div>
                 )}
             </div>
--- task/src/hooks/useFetch.js
+++ task/src/hooks/useFetch.js
@@ -1,26 +1,18 @@
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 const baseURL = 'https://jsonplaceholder.typicode.com'
 
-function useFetch(endpoint, setUsers, setPosts, setComments){
+function useFetch(endpoint){
 
-    console.log(endpoint, typeof endpoint);
-        useEffect(() => {
-            fetch(baseURL + endpoint)
-                .then(response => response.json())
-                .then(data =>
-                {
-                    if (endpoint === '/users') {
-                        setUsers(data)
-                    }else if (endpoint === '/posts') {
-                        setPosts(data)
-                    }if (endpoint === '/comments') {
-                        setComments(data)
-                    }
-                })
-                .catch(error => {
-                    console.error('There was an error!', error);
-                });
-        }, [endpoint])
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        fetch(baseURL + endpoint)
+            .then(response => response.json())
+            .then(data => setData(data))
+            .catch(error => {
+                console.error('There was an error!', error);
+            });}, [endpoint])
+    return data
 }
 export default useFetch
